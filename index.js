0214cd6dbbe0c6574e42201efc1347e9bfbaee1a
Raynald <raynald.gitau@student.moringaschool.com>
2023-03-20 05:55:12 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 * num2);
+console.log(multiply);
 
-// Write your code here
+const random = (Math.floor(Math.random() * 10));
+console.log(random)
+
+const num3 = 29;
+const numb4 = 5;
+const mod = (numb3 %= numb4)
+console.log(mod)
+
+const list = [16,17,4,3,20];
+const max = Math.max (...list);
+console.log(max)
